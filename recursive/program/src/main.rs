03dc4d35f45e778748d93d63426a69dc0f71838b
Aditya Bisht <adityabisht64@gmail.com>
2024-08-14 08:24:23 +0000

    update

--- recursive/program/src/main.rs
+++ recursive/program/src/main.rs
@@ -2,8 +2,8 @@
 sp1_zkvm::entrypoint!(main);
 
 use ark_bn254::Fr;
+use gnark_bn254_verifier::{verify, ProvingSystem};
 use num_bigint::BigUint;
-use plonk_verifier::verify;
 
 pub fn main() {
     let proof = sp1_zkvm::io::read_vec()[8..].to_vec();
@@ -12,7 +12,12 @@ pub fn main() {
     let committed_values_digest = Fr::from(BigUint::from_bytes_be(&sp1_zkvm::io::read_vec()[8..]));
 
     println!("cycle-tracker-start: setup");
-    if verify(&proof, &vk, &[vkey_hash, committed_values_digest]) {
+    if verify(
+        &proof,
+        &vk,
+        &[vkey_hash, committed_values_digest],
+        ProvingSystem::Plonk,
+    ) {
         println!("Proof is valid");
     } else {
         println!("Proof is invalid");
